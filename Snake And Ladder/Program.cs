f4a40a56eb51cf2d2973be40e1bef57540991123
AkshayKumarTN <tnak369@gmail.com>
2021-06-29 12:05:39 +0530

    [Akshay] palyer position

--- Snake And Ladder/Program.cs
+++ Snake And Ladder/Program.cs
@@ -4,9 +4,16 @@ namespace Snake_And_Ladder
 {
     class Program
     {
+        // Starting position in the game is 0
+        //The Player starts from 0 and palys the game till the winning spot 100 is achieved.
+        public const int STAST = 0;
+        public const int FINISH = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome To Snake and Ladder game.");
+            
+            int player_position = 0;
+            Console.WriteLine("player current position is " + player_position);
         }
     }
 }
