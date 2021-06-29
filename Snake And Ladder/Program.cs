0535c2bc2dd86382458098d52520d89a0c40a56e
AkshayKumarTN <tnak369@gmail.com>
2021-06-29 11:48:38 +0530

    [Akshay] initial

---
+++ Snake And Ladder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_And_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome To Snake and Ladder game.");
+        }
+    }
+}
